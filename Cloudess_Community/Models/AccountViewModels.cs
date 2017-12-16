89ae2b264df7557f242580f0de01ec9b8a7515a6
Dora Duszin <dorkodusyn@gmail.com>
2017-12-16 13:58:15 +0100

    Add project files.

---
+++ Cloudess_Community/Models/AccountViewModels.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cloudess_Community.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+
+    public class SendCodeViewModel
+    {
+        public string SelectedProvider { get; set; }
+        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+        public string ReturnUrl { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class VerifyCodeViewModel
+    {
+        [Required]
+        public string Provider { get; set; }
+
+        [Required]
+        [Display(Name = "Code")]
+        public string Code { get; set; }
+        public string ReturnUrl { get; set; }
+
+        [Display(Name = "Remember this browser?")]
+        public bool RememberBrowser { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Jelszó")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Jelszó megerősítése")]
+        [Compare("Password", ErrorMessage = "A két jelszó nem egyezik meg.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class RegisterPartnerViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "e-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "cég neve")]
+        public string companyName { get; set; }
+
+        [Required]
+        [Display(Name = "cég mérete")]
+        public string companySize { get; set; }
+
+        [Required]
+        [Display(Name = "ágazat")]
+        public string field { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "jelszó")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "jelszó újra")]
+        [Compare("Password", ErrorMessage = "A két jelszó nem egyezik meg.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class RegisterUserViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "e-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "érdeklödési kör")]
+        public string interests { get; set; }
+
+        [Required]
+        [Display(Name = "jelenlegi pozíció")]
+        public string actualPosition { get; set; }
+
+        [Required]
+        [Display(Name = "digitális íráskészség")]
+        public string digitalKnowledge { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "jelszó")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "jelszó újra")]
+        [Compare("Password", ErrorMessage = "A két jelszó nem egyezik meg.")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class ResetPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
---
+++ Cloudess_Community/Scripts/site-script.js
@@ -0,0 +1,25 @@
+﻿
+
+
+$(document).ready(function () {
+    //console.log("ready!");
+    //$(window).scroll(function () {
+    //    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    //    if (scrollTop < 100) {
+    //        $('div.BodyContainer.active').removeClass('active').stop(true, true).fadeOut();
+    //    }
+    //    else {
+    //        $('div.BodyContainer').addClass('active').stop(true, true).fadeIn();
+    //    }
+    //});
+    //console.log("ready!");
+    //$('content').css({ 'opacity': (100 - $(window).scrollTop()) / 100 });
+});
+
+
+
+
+
+
+
+
